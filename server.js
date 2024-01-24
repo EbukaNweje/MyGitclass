d15374c2b197daee766edd5170b9de9bb9a79abf
IsaacNdulue <Izikmessiah@gmail.com>
2024-01-24 12:01:49 +0100

    isaac second push

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 mongoose.connect(url).then(() => {
     console.log('Connection to database successful');
 }).then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is listening to port: ${process.env.PORT}`);
+    app.listen(port, () => {
+        console.log(`Server is listening to port: ${port}`);
     })
 })
