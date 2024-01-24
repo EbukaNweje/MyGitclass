488f5349e1303a0330e5b2d4ba7a0ecce4c450ef
eflexTech <eflexmedia1@gmail.com>
2024-01-24 03:07:51 -0800

    GF

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require ('mongoose')
 const port = 3000
+const port2 = 3000
 
 const app = express()
 app.use(express.json())
+console.log(port2)
 
 mongoose.connect(url).then(() => {
     console.log('Connection to database successful');
